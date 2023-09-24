d12694be291490c192495bbeaa5e4fcac5b91bd8
Ace0777 <luishhasantos@gmail.com>
2023-09-23 22:20:33 -0300

    Configurando Arquivo Persistence e xml do banco, configurando e criando classe para ultilizar o metodo entitymanager e criando aluno alunoDao com alguns metodos a fim de teste

--- sistema-controle-doacao/src/main/java/org/sdp/Main.java
+++ sistema-controle-doacao/src/main/java/org/sdp/Main.java
@@ -1,7 +1,25 @@
 package org.sdp;
 
+import org.sdp.banco.dao.UsuarioDao;
+import org.sdp.model.Usuario;
+import org.sdp.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManager em = JPAUtil.getEntityManager();
+
+        UsuarioDao usuarioDao = new UsuarioDao(em);
+
+
+        Usuario usuario = new Usuario("zezin","123");
+
+        em.getTransaction().begin();
+        Usuario u = usuarioDao.buscarPorId(1l);
+        System.out.println("Login: " + u.getLogin() + " Senha: " + u.getSenha());
+        em.getTransaction().commit();
+        em.close();
+
     }
 }
